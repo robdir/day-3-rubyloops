dbe691838328529b27c602de934ef51ffdb08ad2
robdir <rhwickham@gmail.com>
2017-10-04 16:26:54 +0200

    Add methods

--- spanishomlettelab.rb
+++ spanishomlettelab.rb
@@ -81,3 +81,44 @@ puts "See below for recipe overview:"
 steps.each_with_index do |step, index|
   puts (index).to_s + ") " + step[:description]
 end
+
+def generic_recipe_step
+  puts "On it!"
+  print_progress_bar
+end
+
+def scrape_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Scraping potato #{counter}"
+    print_progress_bar
+  end
+end
+
+def cut_potatoes
+  counter = 0
+  while counter < NUM_POTATOES
+    counter += 1
+    print "Cutting potato #{counter} into thick slices"
+    print_progress_bar
+  end
+end
+
+def add_potatoes_to_pan
+  for counter in 1..NUM_POTATOES
+    print "Adding potato #{counter} to the pan."
+    print_progress_bar
+  end
+end
+
+def break_eggs
+  counter = 0
+  while counter < NUM_EGGS
+    counter += 1
+    print "Breaking egg #{counter}"
+    print_progress_bar
+  end
+end
+
+scrape_potatoes
