3da929af30923441bfb6a948a47021413f1c9152
robdir <rhwickham@gmail.com>
2017-10-04 16:03:46 +0200

    print list

--- spanishomlettelab.rb
+++ spanishomlettelab.rb
@@ -14,10 +14,23 @@ end
 NUM_POTATOES = 5
 NUM_EGGS = 6
 
+#Array of hashes for the ingredients
+
 ingredients = [
   { name: 'potatoes', quantity: NUM_POTATOES },
   { name: 'eggs', quantity: NUM_EGGS },
   { name: 'onion', quantity: 1 },
-  { name: 'oil' , quantity: 'some' }
-  { name: 'salt', quantity: 'some'}
+  { name: 'oil' , quantity: 'some' },
+  { name: 'salt', quantity: 'some'},
 ]
+
+#Printing receipe by calling .each on the array of hashes.
+
+puts "*   Let's cook a Spanish Omelette!    *"
+print_divider
+
+puts "You will need the following: "
+
+ingredients.each do |ingredient|
+  puts "* #{ingredient[:quantity]} #{ingredient[:name]}"
+end
