a303f120336ac624aad9c3579340519e0b55d954
robdir <rhwickham@gmail.com>
2017-10-04 17:38:10 +0200

    first version

--- numbergame.rb
+++ numbergame.rb
@@ -3,12 +3,40 @@ def print_divider
   puts "\n"
 end
 
+def guesses_rem
+  puts "You have #{} remaining"
+end
+
 puts "\n \n"
 
 print_divider
 
-puts "Welcome to this number guessing game, please enter a number to begin!"
-user_number = gets.chomp.to_i
+#Variable to be used - random number.
 
-random_number = rand(100)
-user_life = 100
+random_number = 1 + rand(100)
+
+#Variable for user guesses
+
+guess = 0
+
+#Variable for no. of turns
+
+count = 0
+
+puts "Welcome to this number guessing game, please enter a number to begin!"
+guess = gets.chomp.to_i
+
+while count < 10
+  puts "Wrong answer, guess again!"
+  count += 1
+  if count > 10
+    puts "You're out of guesses, Game Over!!"
+  end
+end
+
+if guess == random_number
+    puts "Congratulations, you win!"
+    break
+else
+    puts guess > secret_num ? 'Lower' : 'Higher'
+end
